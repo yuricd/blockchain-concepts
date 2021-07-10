d5c3de96ffcbf8801f0749a2f0dd6088090768ff
Yuri Delgado <yuridelg@gmail.com>
2021-07-09 23:08:50 -0300

    Test add block method

--- blockchain.py
+++ blockchain.py
@@ -6,7 +6,7 @@ class Blockchain:
     def __init__(self):
         self.chain = [Block(index=0, transactions=[],
                             timestamp=time.time(), previous_hash=0)]
-        self.unconfirmed_tx = []
+        self.unconfirmed_txs = []
 
     @property
     def last_block(self):
@@ -14,50 +14,46 @@ class Blockchain:
 
     def pow(self, block, difficulty=3):
         proof_hash = block.hash()
-        while not self.is_valid_proof(block, difficulty=difficulty):
+        while not self.is_valid_proof(block, proof_hash, difficulty=difficulty):
             block.proof += 1
             proof_hash = block.hash()
         return proof_hash
 
-    def add_block(self, block, proof):
+    def add_block(self, block, proof_hash):
         previous_hash = self.last_block.hash()
 
         if previous_hash != block.previous_hash:
             return False
 
-        if not self.is_valid_proof(block, proof):
+        if not self.is_valid_proof(block, proof_hash):
             return False
 
-        block.hash = proof
         self.chain.append(block)
         return True
 
-    def is_valid_proof(self, block, difficulty=3):
-        return block.hash().startswith('0' * difficulty)
+    def is_valid_proof(self, block, proof_hash, difficulty=3):
+        return block.hash().startswith('0' * difficulty) and block.hash() == proof_hash
 
-    def add_new_transaction(self, transaction):
-        self.unconfirmed_tx.append(transaction)
+    def add_transaction(self, transaction):
+        self.unconfirmed_txs.append(transaction)
+
+    def build_transaction(self, sender, receiver, amount):
+        return dict(sender=sender, receiver=receiver, amount=amount)
 
     def mine(self):
-        if not self.unconfirmed_tx:
+        if not self.unconfirmed_txs:
             return False
 
-        last_block = self.last_block
-
         new_block = Block(index=self.index_1,
-                          transactions=self.unconfirmed_tx,
+                          transactions=self.unconfirmed_txs,
                           timestamp=time.time(),
-                          previous_hash=last_block.hash)
+                          previous_hash=self.last_block.hash())
 
-        computed_hash = self.pow(new_block)
-        self.add_block(new_block, computed_hash)
-        self.unconfirmed_tx = []
+        proof_hash = self.pow(new_block)
+        self.add_block(new_block, proof_hash)
+        self.unconfirmed_txs = []
         return new_block.index
 
     def print(self):
         for block in self.chain:
             block.print()
-
-
-chain = Blockchain()
-print(chain.print())
--- test_blockchain.py
+++ test_blockchain.py
@@ -4,11 +4,10 @@ import unittest
 from json import dumps
 from copy import deepcopy
 
-
 index = 1
 transactions = ['a', 'b']
 timestamp = 'ts'
-previous_hash = 'b'
+previous_hash = 0
 proof = 100
 sample_block = Block(index, transactions, timestamp, previous_hash, proof)
 
@@ -21,34 +20,79 @@ class TestBlockchain(unittest.TestCase):
     def test_pow_success(self):
         blockchain = Blockchain()
         block = deepcopy(sample_block)
-        computed_hash = blockchain.pow(block)
+        proof_hash = blockchain.pow(block)
 
-        self.assertEqual(blockchain.is_valid_proof(block), True,
+        self.assertEqual(blockchain.is_valid_proof(block, proof_hash), True,
                          'Should apply PoW algorithm successfully')
 
     def test_pow_failure(self):
         blockchain = Blockchain()
         block = deepcopy(sample_block)
-        computed_hash = blockchain.pow(block)
+        proof_hash = blockchain.pow(block)
         block.proof += 1
-        self.assertEqual(blockchain.is_valid_proof(block), False,
+        self.assertEqual(blockchain.is_valid_proof(block, proof_hash), False,
                          'Should apply PoW algorithm and failed due to wrong proof counter')
 
     def test_is_valid_proof(self):
         blockchain = Blockchain()
         block = deepcopy(sample_block)
-        block.proof = 6271  # proof for difficulty 3
 
-        valid = blockchain.is_valid_proof(block, difficulty=3)
+        # proof for difficulty 3 in sample block
+        proof_hash = blockchain.pow(block)
+
+        valid = blockchain.is_valid_proof(block, proof_hash, difficulty=3)
 
         self.assertEqual(valid, True,
                          'Should be valid')
 
-    def test_add_new_transaction(self):
-        pass
+    def test_add_transaction(self):
+        blockchain = Blockchain()
+        new_tx = blockchain.build_transaction(
+            'SenderAddress', 'ReceiverAddress', 10)
+
+        self.assertEqual(blockchain.unconfirmed_txs, [],
+                         'Should return empty unconfirmed transaction array')
+
+        blockchain.add_transaction(new_tx)
+
+        self.assertEqual(blockchain.unconfirmed_txs, [
+                         new_tx], 'Should return 1 unconfirmed transaction in array')
+
+    def test_add_block(self):
+        blockchain = Blockchain()
+        block = deepcopy(sample_block)
+        block.previous_hash = blockchain.chain[0].hash()
+
+        proof_hash = blockchain.pow(block)
+
+        self.assertEqual(len(blockchain.chain), 1,
+                         'Should return only genesis block before adding second block')
+
+        was_added = blockchain.add_block(block, proof_hash)
+
+        self.assertEqual(
+            was_added, True, 'Should return true when add a valid block')
+
+        self.assertEqual(len(blockchain.chain), 2,
+                         'Should return only genesis block before adding second block')
 
     def test_mine(self):
-        pass
+        blockchain = Blockchain()
+        block = deepcopy(sample_block)
+        block.previous_hash = blockchain.chain[0].hash()
+
+        proof_hash = blockchain.pow(block)
+
+        self.assertEqual(len(blockchain.chain), 1,
+                         'Should return only genesis block before adding second block')
+
+        was_added = blockchain.add_block(block, proof_hash)
+
+        self.assertEqual(
+            was_added, True, 'Should return true when add a valid block')
+
+        self.assertEqual(len(blockchain.chain), 2,
+                         'Should return only genesis block before adding second block')
 
 
 if __name__ == '__main__':
